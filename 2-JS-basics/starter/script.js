fe73011ef7a1a12cff78536a5c1e75e44f1ab95d
Jihin Ip <jim_ip20@hotmail.com>
2019-02-19 13:58:12 +0000

    more practice stuff

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,21 @@
+console.log("Hello world.");
+
+var firstName = 'John';
+var lastName = 'Doe';
+var age = 28;
+
+var fullAge = true;
+
+
+// we can also create multiple variables on a single line:
+
+var job, isMarried;
+job = 'teacher';
+isMarried = 'no';
+
+var lastName = prompt('What is his last name?');
+
+console.log(firstName + ' ' + lastName + ' is ' + age + ' years old.');
+
+// This will print out what the object type is.
+console.log(typeof fullAge);
